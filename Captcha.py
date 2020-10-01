44458fcb8431adfcde306eec4a9759b5f875f9e7
rosem <8cr3faqxd1f7@mail.ru>
2020-10-01 23:39:13 +0300

    загружен скрипт Captcha.py для тестирования капчи для роботов

---
+++ Captcha.py
@@ -0,0 +1,32 @@
+﻿import math
+def calc(x):
+  return str(math.log(abs(12*math.sin(int(x)))))
+from selenium import webdriver
+import time
+
+try:
+    link = "http://suninjuly.github.io/math.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    x_element = browser.find_element_by_id("input_value")
+    x = x_element.text
+    y = calc(x)
+
+    input = browser.find_element_by_id("answer")
+    input.send_keys(y)
+    
+    checkbox = browser.find_element_by_id("robotCheckbox")
+    checkbox.click()
+    
+    radiobutton = browser.find_element_by_id("robotsRule")
+    radiobutton.click()
+
+    button = browser.find_element_by_css_selector(".btn")
+    button.click()
+
+finally:
+    
+    time.sleep(10)
+    
+    browser.quit()
